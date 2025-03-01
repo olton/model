d29eced4ba421321f7d450937a60c98c8dfc0f62
Serhii Pimenov <serhii@pimenov.com.ua>
2025-03-01 19:49:28 +0200

    improves: add directives m-for, m-if, arrays, nested objects

--- dist/model.js
+++ dist/model.js
@@ -1,7 +1,7 @@
 
 /*!
  * Model v0.3.0
- * Build: 01.03.2025, 10:52:00
+ * Build: 01.03.2025, 19:49:19
  * Copyright 2012-2025 by Serhii Pimenov
  * Licensed under MIT
  */
@@ -15,7 +15,6 @@ var Model = class _Model {
   static DEBUG = false;
   static log = (...args) => {
     if (_Model.DEBUG) {
-      console.log(...args);
     }
   };
   constructor(data = {}, options = {}) {
@@ -49,7 +48,6 @@ var Model = class _Model {
       _Model.log(`\u041E\u0431\u0440\u043E\u0431\u043A\u0430 \u0435\u043B\u0435\u043C\u0435\u043D\u0442\u0443 ${index}:`, expression);
       const matches = expression.match(/^\s*(\w+)(?:\s*,\s*(\w+))?\s+in\s+(\w+(?:\.\w+)*)\s*$/);
       if (!matches) {
-        console.error("\u041D\u0435\u043F\u0440\u0430\u0432\u0438\u043B\u044C\u043D\u0438\u0439 \u0444\u043E\u0440\u043C\u0430\u0442 \u0432\u0438\u0440\u0430\u0437\u0443 m-for:", expression);
         return;
       }
       const [_, itemName, indexName, arrayPath] = matches;
@@ -57,7 +55,6 @@ var Model = class _Model {
       const array = this.getValueByPath(arrayPath);
       _Model.log("\u041E\u0442\u0440\u0438\u043C\u0430\u043D\u043E \u043C\u0430\u0441\u0438\u0432:", array);
       if (!Array.isArray(array)) {
-        console.error(`\u0417\u043D\u0430\u0447\u0435\u043D\u043D\u044F \u0437\u0430 \u0448\u043B\u044F\u0445\u043E\u043C ${arrayPath} \u043D\u0435 \u0454 \u043C\u0430\u0441\u0438\u0432\u043E\u043C:`, array);
         return;
       }
       const template = element.cloneNode(true);
@@ -75,14 +72,12 @@ var Model = class _Model {
   updateLoop(element) {
     const loopInfo = this.loops.get(element);
     if (!loopInfo) {
-      console.error("\u041D\u0435 \u0437\u043D\u0430\u0439\u0434\u0435\u043D\u043E \u0456\u043D\u0444\u043E\u0440\u043C\u0430\u0446\u0456\u044E \u043F\u0440\u043E \u0446\u0438\u043A\u043B \u0434\u043B\u044F \u0435\u043B\u0435\u043C\u0435\u043D\u0442\u0443");
       return;
     }
     const { template, itemName, indexName, arrayPath, parentNode } = loopInfo;
     const array = this.getValueByPath(arrayPath);
     _Model.log("\u041E\u043D\u043E\u0432\u043B\u0435\u043D\u043D\u044F \u0446\u0438\u043A\u043B\u0443 \u0434\u043B\u044F \u043C\u0430\u0441\u0438\u0432\u0443:", array);
     if (!Array.isArray(array)) {
-      console.error("\u0417\u043D\u0430\u0447\u0435\u043D\u043D\u044F \u043D\u0435 \u0454 \u043C\u0430\u0441\u0438\u0432\u043E\u043C:", array);
       return;
     }
     const generated = parentNode.querySelectorAll(`[data-generated-for="${arrayPath}"]`);
@@ -374,7 +369,6 @@ var Model = class _Model {
     const parts = path.split(".");
     for (const part of parts) {
       if (value === void 0 || value === null) {
-        console.error(`\u0428\u043B\u044F\u0445 ${path} \u043E\u0431\u0456\u0440\u0432\u0430\u0432\u0441\u044F \u043D\u0430 ${part}`);
         return void 0;
       }
       value = value[part];
@@ -412,7 +406,6 @@ var Model = class _Model {
           element.style.display = result ? originalDisplay || "" : "none";
           _Model.log(`\u0420\u0435\u0437\u0443\u043B\u044C\u0442\u0430\u0442 \u0432\u0438\u0440\u0430\u0437\u0443 ${expression}:`, result);
         } catch (error) {
-          console.error("\u041F\u043E\u043C\u0438\u043B\u043A\u0430 \u043F\u0440\u0438 \u043E\u0431\u0440\u043E\u0431\u0446\u0456 m-if:", error);
         }
       };
       const variables = this.extractVariables(expression);
@@ -433,7 +426,6 @@ var Model = class _Model {
       const func = new Function(...Object.keys(context), `return ${expression}`);
       return func(...Object.values(context));
     } catch (error) {
-      console.error("\u041F\u043E\u043C\u0438\u043B\u043A\u0430 \u043F\u0440\u0438 \u043E\u0446\u0456\u043D\u0446\u0456 \u0432\u0438\u0440\u0430\u0437\u0443:", error);
       return false;
     }
   }
@@ -441,7 +433,6 @@ var Model = class _Model {
   init(selector) {
     const rootElement = typeof selector === "string" ? document.querySelector(selector) : selector;
     if (!rootElement) {
-      console.error("The root element was not found!");
       return;
     }
     this.parseLoops(rootElement);
@@ -454,10 +445,7 @@ var Model = class _Model {
 var model_default = Model;
 
 // src/index.js
-var version = "___VERSION___";
-var build_time = "___BUILD_TIME___";
 model_default.info = () => {
-  console.info(`%c Dom %c v${version} %c ${build_time} `, "color: white; font-weight: bold; background: #0080fe", "color: white; background: darkgreen", "color: white; background: #0080fe;");
 };
 var index_default = model_default;
 export {
