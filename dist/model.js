d70cb17d601b813f02813532f5ed357dccd95f0b
Serhii Pimenov <serhii@pimenov.com.ua>
2025-03-02 00:39:46 +0200

    improve dev-tools

--- dist/model.js
+++ dist/model.js
@@ -1,7 +1,7 @@
 
 /*!
- * Model v0.6.0
- * Build: 01.03.2025, 23:31:12
+ * Model v0.7.0
+ * Build: 02.03.2025, 00:39:42
  * Copyright 2012-2025 by Serhii Pimenov
  * Licensed under MIT
  */
@@ -980,8 +980,8 @@ var Model = class _Model extends event_emmiter_default {
 var model_default = Model;
 
 // src/index.js
-var version = "0.6.0";
-var build_time = "01.03.2025, 23:31:12";
+var version = "0.7.0";
+var build_time = "02.03.2025, 00:39:42";
 model_default.info = () => {
   console.info(`%c Model %c v${version} %c ${build_time} `, "color: white; font-weight: bold; background: #0080fe", "color: white; background: darkgreen", "color: white; background: #0080fe;");
 };
